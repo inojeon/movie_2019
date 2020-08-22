57816126fcc79dfdb6ba3f16bfb7c792dd0def20
inojeon <inojeon@kisti.re.kr>
2020-08-22 17:07:59 +0900

    Updates

---
+++ precache-manifest.2737dffcef134dee4fa5dffa31d5214d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4849b92d6666c2574fd723dd957d620",
+    "url": "/movie_2019/index.html"
+  },
+  {
+    "revision": "b0739e45f2313babc29e",
+    "url": "/movie_2019/static/css/main.a96284d9.chunk.css"
+  },
+  {
+    "revision": "76833cf627d0b114f6c5",
+    "url": "/movie_2019/static/js/2.996bed0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_2019/static/js/2.996bed0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0739e45f2313babc29e",
+    "url": "/movie_2019/static/js/main.ebfa36a0.chunk.js"
+  },
+  {
+    "revision": "a1aa4805be0f9bc5343f",
+    "url": "/movie_2019/static/js/runtime-main.1e9860eb.js"
+  }
+]);
